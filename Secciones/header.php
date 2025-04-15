5c811f71370047a8f223e3a429c9aabb9b2a1944
fonsekorz <fonsekaov84@proton.me>
2025-04-15 20:29:15 +0200

    vamos a añadir boton index

--- Secciones/header.php
+++ Secciones/header.php
@@ -9,6 +9,7 @@ $dato = $datos[0];
     </div>
     <button id="menu-toggle" aria-label="Abrir menú de navegación">&#9776;</button>
     <nav id="header-nav">
+        <a href="./index.php"><img src="assets/home.svg" alt="Botono Inicio"></a>
         <a href="./index.php"><?= $dato['about'] ?></a>
         <a href="./proyecto.php"><?= $dato['proyects'] ?></a>
         <a href="./contacto.php"><?= $dato['contact'] ?></a>
