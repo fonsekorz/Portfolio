5084a11e2fa0c5bfbdedc8309ab69e3e27064222
fonsekorz <fonsekaov84@proton.me>
2025-04-23 16:02:15 +0200

    cambios

--- Secciones/header.php
+++ Secciones/header.php
@@ -9,10 +9,9 @@ $dato = $datos[0];
     </div>
     <button id="menu-toggle" aria-label="Abrir menú de navegación">&#9776;</button>
     <nav id="header-nav">
-        <a href="./index.php"><img src="assets/home.svg" alt="Botono Inicio"></a>
-        <a href="./index.php"><?= $dato['about'] ?></a>
         <a href="./proyecto.php"><?= $dato['proyects'] ?></a>
         <a href="./contacto.php"><?= $dato['contact'] ?></a>
+        <a href="./index.php"><img src="assets/home.svg" alt="Botono Inicio"></a>
     </nav>
 </header>
 <?php
