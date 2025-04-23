d0e7b7b1ce1a516397519a546633d417bc7e4353
fonsekorz <fonsekaov84@proton.me>
2025-04-23 18:44:34 +0200

    logo como url a index

--- Secciones/header.php
+++ Secciones/header.php
@@ -5,13 +5,12 @@ $dato = $datos[0];
 ?>
 <header id="header-container">
     <div id="header-logo">
-        <img src="<?= $dato['logo'] ?>" alt="Logo Fonseka" />
+    <a href="./index.php"><img src="<?= $dato['logo'] ?>" alt="Logo Fonseka" /></a>
     </div>
     <button id="menu-toggle" aria-label="Abrir menú de navegación">&#9776;</button>
     <nav id="header-nav">
         <a href="./proyecto.php"><?= $dato['proyects'] ?></a>
         <a href="./contacto.php"><?= $dato['contact'] ?></a>
-        <a href="./index.php"><img src="assets/home.svg" alt="Botono Inicio"></a>
     </nav>
 </header>
 <?php
